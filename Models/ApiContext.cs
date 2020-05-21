56b87b1d110a5c55ca73c12acc66fce7bf4c421b
admin <annanihao@yandex.ru>
2020-05-21 09:41:34 +0800

    Implement DataSeeder

--- Models/ApiContext.cs
+++ Models/ApiContext.cs
@@ -1,15 +1,14 @@
-using Microsoft.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 
 namespace Advantage.API.Models
 {
     public class ApiContext: DbContext
     {
-        public ApiContext(DbContextOptions<ApiContext>options) : base(options){}
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options){}
 
-        public DbSet <Custmer>Customers {get;set;}
-        public DbSet <Order>Orders {get;set;}
-        public DbSet <Server>Servers {get;set;}
+        public DbSet <Customer> Customers {get;set;}
+        public DbSet <Order> Orders {get;set;}
+        public DbSet <Server> Servers {get;set;}
 
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Advantage.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace Advantage.API
 {
     public class Startup
     {
+        private string _connectionString = null;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,18 +27,29 @@ namespace Advantage.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            _connectionString = Configuration["secretConnectionString"];
+
             services.AddMvc();
+
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ApiContext>(
+                opt=>opt.UseNpgsql(_connectionString));
+
+            services.AddTransient<DataSeed>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DataSeed seeder)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            seeder.SeedData(20,1000);
+
             app.UseMvc();
+
         }
     }
 }
